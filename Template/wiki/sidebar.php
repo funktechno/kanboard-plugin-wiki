7df54b991a0237aadfdd5176028890b7e0629916
lastlink <theefunk@gmail.com>
2018-02-14 01:36:35 -0500

    hide from sidebar

--- Template/wiki/sidebar.php
+++ Template/wiki/sidebar.php
@@ -6,14 +6,9 @@
         <li <?= $this->app->checkMenuSelection('WikiLineController', 'show') ?>>
             <?= $this->url->link(t('Wiki lines'), 'WikiLineController', 'show', array('plugin' => 'wiki', 'project_id' => $project['id'])) ?>
         </li>
-        <?php 
-            $wikipages = array(
-                 "title"=>"candy"
-             ,
-                 "title"=>"bob"  
-            );
-                // $this->wikiHelper->getWikipages($project['id']) 
-        ?>
+        <li>
+            <?= $this->modal->medium('file', t('Attach a document'), 'WikiFileController', 'create', array('wiki_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
+        </li>
 
         <?php if (! empty($wikipages)): ?>
         <?php foreach ($wikipages as $wikipage): ?>
@@ -24,10 +19,10 @@
 
        
         <?php endforeach ?>
-        <?php else: ?>
-        <li class="alert alert-info">
-            <?= t('There are no wikipages.') ?>
-        </li>
+        <?php //else: ?>
+        <!-- <li class="alert alert-info"> -->
+            <!--  //t('There are no wikipages.')  -->
+        <!-- </li> -->
         <?php endif ?>
 
     </ul>
