48338ac9293a980bdd32643749c781db948980d7
lastlink <theefunk@gmail.com>
2018-02-13 23:14:33 -0500

    remove user sidebar

--- Template/user/sidebar.php
+++
@@ -1,5 +0,0 @@
-<?php if ($this->user->hasAccess('HourlyRateController', 'index')): ?>
-    <li <?= $this->app->getRouterController() === 'HourlyRateController' ? 'class="active"' : '' ?>>
-        <?= $this->url->link(t('Hourly rate'), 'HourlyRateController', 'show', array('plugin' => 'wiki', 'user_id' => $user['id'])) ?>
-    </li>
-<?php endif ?>
