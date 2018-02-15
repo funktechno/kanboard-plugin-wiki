7ff03379a4bc4d774f27764520c1b9c00d9ad355
lastlink <theefunk@gmail.com>
2018-02-15 10:11:08 -0500

    attempt id again

--- Template/wiki/edit.php
+++ Template/wiki/edit.php
@@ -4,13 +4,15 @@
 
 <form method="post" action="<?=$this->url->href('WikiController', 'update', array('plugin' => 'wiki'))?>" autocomplete="off">
     <?=$this->form->csrf()?>
+    <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    ?>
     
     <?=$this->form->hidden('id', $values)?>
     <?=$this->form->hidden('editions', $values)?>
     <?=$this->form->hidden('order', $values)?>
-    <?=$values['id']?>
-    <?=$values['editions']?>
-    <?=$values['order']?>
 
     <?=$this->form->label(t('Title'), 'title')?>
     <?=$this->form->text('title', $values, $errors, array('required', 'maxlength="255"', 'autofocus', 'tabindex="1"'))?>
