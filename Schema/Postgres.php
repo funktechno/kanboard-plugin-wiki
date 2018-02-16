0a7200ffc04e645a18a9156fed4a3dce05230b70
lastlink <theefunk@gmail.com>
2018-02-16 10:55:37 -0500

    test and update schema for postgress & sqllite

--- Schema/Postgres.php
+++ Schema/Postgres.php
@@ -8,42 +8,42 @@ const VERSION = 1;
 
 function version_1(PDO $pdo)
 {
-    $pdo->exec("CREATE TABLE IF NOT EXISTS wikipage (
-        `id` SERIAL PRIMARY KEY,
-        `project_id` INTEGER NOT NULL,
-        `title` varchar(255) NOT NULL,
-        `content` TEXT,
-        `is_active` tinyint(4) DEFAULT 1,
-        `creator_id` int(11) DEFAULT 0,
-        `modifier_id` int(11) DEFAULT 0;
-        `date_creation` VARCHAR(10) DEFAULT NULL,
-        `date_modification` VARCHAR(10) DEFAULT NULL;
-        `order` int(11) DEFAULT 1,
-        `editions` int default 1;
-        `current_edition` int default 1;
+    $pdo->exec('CREATE TABLE IF NOT EXISTS wikipage (
+        id SERIAL PRIMARY KEY,
+        project_id INTEGER NOT NULL,
+        title varchar(255) NOT NULL,
+        content TEXT,
+        is_active smallint DEFAULT 1,
+        creator_id INTEGER DEFAULT 0,
+        modifier_id INTEGER DEFAULT 0,
+        date_creation VARCHAR(10) DEFAULT NULL,
+        date_modification VARCHAR(10) DEFAULT NULL,
+        "order" INTEGER DEFAULT 1,
+        editions INTEGER default 1,
+        current_edition INTEGER default 1,
         FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
-    );");
+    );');
 
     $pdo->exec('CREATE TABLE wikipage_has_files (
         "id" SERIAL PRIMARY KEY,
         name VARCHAR(50),
         path VARCHAR(255),
-        is_image TINYINT(1) DEFAULT 0,
-        wikipage_id INT,
+        is_image smallint DEFAULT 0,
+        wikipage_id INTEGER,
         FOREIGN KEY(wikipage_id) REFERENCES wikipage(id) ON DELETE CASCADE
     )');
 
     $pdo->exec('CREATE TABLE wikipage_editions (
-    `edition` INT NOT NULL,
-    `title` varchar(255) NOT NULL,
-    `content` TEXT,
-    `creator_id` int(11) DEFAULT 0,
-    `date_creation` VARCHAR(10) DEFAULT NULL,
-    wikipage_id INT NOT NULL,
-    PRIMARY KEY (`edition`,`wikipage_id`),
+    "edition" INT NOT NULL,
+    "title" varchar(255) NOT NULL,
+    "content" TEXT,
+    "creator_id" INTEGER DEFAULT 0,
+    "date_creation" VARCHAR(10) DEFAULT NULL,
+    wikipage_id INTEGER NOT NULL,
+    PRIMARY KEY ("edition","wikipage_id"),
     FOREIGN KEY(wikipage_id) REFERENCES wikipage(id) ON DELETE CASCADE
     )');
 
-    $pdo->exec("INSERT INTO `settings` (`option`, `value`) VALUES ('persistEditions', '1');");
+    $pdo->exec('INSERT INTO settings (option, value) VALUES ('persistEditions', '1');');
 
 }
--- Schema/Sqlite.php
+++ Schema/Sqlite.php
@@ -8,27 +8,27 @@ const VERSION = 1;
 
 function version_1(PDO $pdo)
 {
-    $pdo->exec("CREATE TABLE IF NOT EXISTS wikipage (
-        `id` INTEGER PRIMARY KEY,
-        `project_id` INTEGER NOT NULL,
-        `title` varchar(255) NOT NULL,
-        `content` TEXT,
-        `is_active` tinyint(4) DEFAULT 1,
-        `creator_id` int(11) DEFAULT 0,
-        `modifier_id` int(11) DEFAULT 0;
-        `date_creation` VARCHAR(10) DEFAULT NULL,
-        `date_modification` VARCHAR(10) DEFAULT NULL;
-        `order` int(11) DEFAULT 1,
-        `editions` int default 1;
-        `current_edition` int default 1;
-        FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
-    );");
+    $pdo->exec("CREATE TABLE `wikipage` (
+	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`project_id`	INTEGER NOT NULL,
+	`title`	varchar(255) NOT NULL,
+	`content`	TEXT DEFAULT 1,
+	`is_active`	int(4) DEFAULT 1,
+	`creator_id`	int(11) DEFAULT 0,
+	`modifier_id`	int(11),
+	`date_creation`	INTEGER,
+	`date_modification`	INTEGER,
+	`order`	INTEGER DEFAULT 1,
+	`editions`	INTEGER DEFAULT 1,
+	`current_edition`	INTEGER DEFAULT 1,
+	FOREIGN KEY(`project_id`) REFERENCES `projects`(`id`)
+);");
 
     $pdo->exec('CREATE TABLE wikipage_has_files (
         "id" INTEGER PRIMARY KEY,
         name VARCHAR(50),
         path VARCHAR(255),
-        is_image TINYINT(1) DEFAULT 0,
+        is_image INTEGER DEFAULT 0,
         wikipage_id INT,
         FOREIGN KEY(wikipage_id) REFERENCES wikipage(id) ON DELETE CASCADE
     )');
