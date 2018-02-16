cc3c6e37adac306824c073bcf9f2aff340251bfd
lastlink <theefunk@gmail.com>
2018-02-16 01:22:11 -0500

    invert config save

--- Template/config/wiki.php
+++ Template/config/wiki.php
@@ -14,16 +14,16 @@
          <!-- $this->form->checkbox('persistEditions','Save Editions', 1, true)  -->
 
         
-        <!-- // $this->form->checkbox('persistEditions', 'Save Editions', 1, $values['persistEditions'] == 1) -->
+         <?=$this->form->checkbox('persistEditions', 'Save Editions', $values['persistEditions'] == 1 ? 0 : 1, $values['persistEditions'] == 1)?>
         
         <!-- <input type="checkbox" name="persistEditions" class="" value="1" checked="checked"> -->
-        <?=`<input name="persistEditions" type="checkbox" disabled="disabled"  value=` . ($values['persistEditions'] == 1 ? "0" : "1") . ($values['persistEditions'] == 1 ? " checked>" : ">")?>
+        <!-- `<input name="persistEditions" type="checkbox" disabled="disabled"  value=` . ($values['persistEditions'] == 1 ? "0" : "1") . ($values['persistEditions'] == 1 ? " checked>" : ">") -->
          <!-- $this->form->checkbox('password_reset', t('Enable "Forget Password"'), 1, $values['password_reset'] == 1) -->
 
 
     </fieldset>
 
     <div class="form-actions">
-        <button type="submit" class="btn btn-blue"><?=t('Switch')?></button>
+        <button type="submit" class="btn btn-blue"><?=t('Save')?></button>
     </div>
 </form>
