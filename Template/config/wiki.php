e52e606e9810f8ef8b0e9cbcaec13b9192cb3d31
lastlink <theefunk@gmail.com>
2018-02-16 01:29:36 -0500

    working config

--- Template/config/wiki.php
+++ Template/config/wiki.php
@@ -7,20 +7,11 @@
 
     <fieldset>
         <legend><?=t('Editions\' settings')?></legend>
-        <?=json_encode($values)?>
-        <?=$values['persistEditions'] == 1?>
 
-
-         <!-- $this->form->checkbox('persistEditions','Save Editions', 1, true)  -->
         <h2>Saving Editions: <?=$values['persistEditions'] == 1 ? "true" : "false" ?></h2>
         
          <?=$this->form->checkbox('persistEditions', 'Switch Edition Saving', $values['persistEditions'] == 1 ? 0 : 1, true)?>
-        
-        <!-- <input type="checkbox" name="persistEditions" class="" value="1" checked="checked"> -->
-        <!-- `<input name="persistEditions" type="checkbox" disabled="disabled"  value=` . ($values['persistEditions'] == 1 ? "0" : "1") . ($values['persistEditions'] == 1 ? " checked>" : ">") -->
-         <!-- $this->form->checkbox('password_reset', t('Enable "Forget Password"'), 1, $values['password_reset'] == 1) -->
-
-
+         
     </fieldset>
 
     <div class="form-actions">
