9838fd9b9502488bdd6cb10230536256066ee994
lastlink <theefunk@gmail.com>
2018-02-16 01:10:32 -0500

    tweak config values params

--- Template/config/wiki.php
+++ Template/config/wiki.php
@@ -14,10 +14,10 @@
          <!-- $this->form->checkbox('persistEditions','Save Editions', 1, true)  -->
 
         
-        // $this->form->checkbox('persistEditions', 'Save Editions', 1, $values['persistEditions'] == 1)
+        <!-- // $this->form->checkbox('persistEditions', 'Save Editions', 1, $values['persistEditions'] == 1) -->
         
         <!-- <input type="checkbox" name="persistEditions" class="" value="1" checked="checked"> -->
-        <?=`<input name="persistEditions" type="checkbox" disabled="disabled"  value=` . ($values['persistEditions'] == 1 ? "1" : "0") . `"  ` . ($values['persistEditions'] == 1 ? "checked" : "")?>
+        <?=`<input name="persistEditions" type="checkbox" disabled="disabled"  value=` . ($values['persistEditions'] == 1 ? "0" : "1") . ($values['persistEditions'] == 1 ? " checked>" : ">")?>
          <!-- $this->form->checkbox('password_reset', t('Enable "Forget Password"'), 1, $values['password_reset'] == 1) -->
 
 
