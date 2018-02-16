529f5aafd9d598d764ce0fb57851997571e1887b
lastlink <theefunk@gmail.com>
2018-02-16 01:08:17 -0500

    tweak config values params

--- Template/config/wiki.php
+++ Template/config/wiki.php
@@ -1,24 +1,29 @@
 <div class="page-header">
-    <h2><?= t('Wiki settings') ?></h2>
+    <h2><?=t('Wiki settings')?></h2>
 </div>
-<form method="post" action="<?= $this->url->href('ConfigController', 'save', array('plugin' => 'Wiki')) ?>" autocomplete="off">
+<form method="post" action="<?=$this->url->href('ConfigController', 'save', array('plugin' => 'Wiki'))?>" autocomplete="off">
 
-    <?= $this->form->csrf() ?>
+    <?=$this->form->csrf()?>
 
     <fieldset>
-        <legend><?= t('Editions\' settings') ?></legend>
-        <?= json_encode($values) ?>
+        <legend><?=t('Editions\' settings')?></legend>
+        <?=json_encode($values)?>
+        <?=$values['persistEditions'] == 1?>
+
 
          <!-- $this->form->checkbox('persistEditions','Save Editions', 1, true)  -->
 
-        <?= $this->form->checkbox('persistEditions','Save Editions', 1, $values['persistEditions'] == 1) ?>
-
+        
+        // $this->form->checkbox('persistEditions', 'Save Editions', 1, $values['persistEditions'] == 1)
+        
+        <!-- <input type="checkbox" name="persistEditions" class="" value="1" checked="checked"> -->
+        <?=`<input name="persistEditions" type="checkbox" disabled="disabled"  value=` . ($values['persistEditions'] == 1 ? "1" : "0") . `"  ` . ($values['persistEditions'] == 1 ? "checked" : "")?>
          <!-- $this->form->checkbox('password_reset', t('Enable "Forget Password"'), 1, $values['password_reset'] == 1) -->
 
-        
+
     </fieldset>
 
     <div class="form-actions">
-        <button type="submit" class="btn btn-blue"><?= t('Save') ?></button>
+        <button type="submit" class="btn btn-blue"><?=t('Switch')?></button>
     </div>
 </form>
