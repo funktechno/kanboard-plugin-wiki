209c4049f628a9cc8dcee017f9e31c06a6dfe676
lastlink <theefunk@gmail.com>
2018-02-21 23:11:40 -0500

    comment out warning

--- Controller/WikiController.php
+++ Controller/WikiController.php
@@ -17,9 +17,9 @@ class WikiController extends BaseController
      */
     public function show()
     {
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
+        // ini_set('display_errors', 1);
+        // ini_set('display_startup_errors', 1);
+        // error_reporting(E_ALL);
 
         $project = $this->getProject();
 
