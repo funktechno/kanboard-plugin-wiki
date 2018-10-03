0c3b82ddc3b7550818942c4875c883752bf4c3d9
Craig Crosby <creecros@gmail.com>
2018-10-03 13:20:20 -0400

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -12,6 +12,7 @@ class Plugin extends Base
     {
         $this->projectAccessMap->add('WikiController', '*', Role::PROJECT_MEMBER);
         $this->projectAccessMap->add('WikiFileController', '*', Role::PROJECT_MEMBER);
+        $this->projectAccessMap->add('WikiFileViewController', '*', Role::PROJECT_MEMBER);
 
         $this->route->addRoute('/wiki/project/:project_id', 'WikiController', 'show', 'wiki');
         $this->route->addRoute('/wiki/project/:project_id', 'WikiController', 'detail', 'wiki');
