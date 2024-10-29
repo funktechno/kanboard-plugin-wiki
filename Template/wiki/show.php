6eb0fd9afad01952cc0cad03f5d011f22bc00f14
Im[F(x)] <imfx77@hotmail.com>
2024-10-29 13:15:58 +0200

    * totally removing clearfix styles from css and from inline * adjusting styles for wiki content list items

--- Template/wiki/show.php
+++ Template/wiki/show.php
@@ -16,30 +16,8 @@
     <?php endif ?>
 </div>
 
-<style>
-    .clearfix::after {
-        content: "";
-        clear: both;
-        display: table;
-    }
-    .column {
-        float: left;
-        min-width: 0;
-    }
-    .list {
-    width: 25%;
-}
-    .content {
-    width: 75%;
-}
-
-</style>
-<div class="clearfix">
-
-<div class="column ">
 <?php if (!empty($wikipages)): ?>
 
-<!-- <hr/> -->
 <!-- Title
 Editions
 Creator
@@ -47,52 +25,48 @@ Created
 Last modifier
 Modified -->
 
-        <table class="table-fixed table-stripped" style="width:100%">
-            <tr>
-                <th style="width:10%"><?=t('Title')?></th>
-                <th style="width:5%"><?=t('Id')?></th>
-                <th style="width:5%"><?=t('is a child of')?></th>
-                <th style="width:5%"><?=t('Editions')?></th>
-                <th style="width:5%"><?=t('Current Edition')?></th>
-                <th style="width:9%"><?=t('Creator')?></th>
-                <th style="width:9%"><?=t('Created')?></th>
-                <th style="width:12%"><?=t('Last modifier')?></th>
-                <th style="width:9%"><?=t('Modified')?></th>
-            </tr>
-            <?php foreach ($wikipages as $wikipage): ?>
-            <tr>
-                <td>
-                <?php if (!$not_editable): ?>
-                    <?=$this->url->link(t($wikipage['title']), 'WikiController', 'detail', array('plugin' => 'wiki', 'project_id' => $project['id'], 'wiki_id' => $wikipage['id']))?>
-                    <?=$this->modal->confirm('trash-o', t(''), 'WikiController', 'confirm', array('plugin' => 'wiki', 'project_id' => $project['id'], 'wiki_id' => $wikipage['id']))?>
-                <?php else: ?>
-                    <?=$this->url->link(t($wikipage['title']), 'WikiController', 'detail_readonly', array('plugin' => 'wiki', 'token' => $project['token'], 'wiki_id' => $wikipage['id']))?>
-                <?php endif ?>
-                </td>
-                <td>
-                    <?=$wikipage['id']?>
-                </td>
-                <td>
-                    <?php if (!$not_editable): ?>
-                    <?=$this->url->link($wikipage['parent_id'], 'WikiController', 'detail', array('plugin' => 'wiki', 'project_id' => $project['id'], 'wiki_id' => $wikipage['parent_id']))?>
-                    <?php else: ?>
-                        <?=$this->url->link($wikipage['parent_id'], 'WikiController', 'detail_readonly', array('plugin' => 'wiki', 'token' => $project['token'], 'wiki_id' => $wikipage['parent_id']))?>
-                    <?php endif ?>
-                </td>
-                <td><?=$wikipage['editions']?></td>
-                <td><?=$wikipage['current_edition']?></td>
-                <td><?=$this->text->e($wikipage['creator_name'] ?: $wikipage['creator_username'])?></td>
-                <td><?=$this->dt->date($wikipage['date_creation'])?></td>
-                <td><?=$this->text->e($wikipage['modifier_name'] ?: $wikipage['modifier_username'])?></td>
-                <td><?=$this->dt->date($wikipage['date_modification'])?></td>
-            </tr>
-            <?php endforeach?>
-        </table>
-    </div>
+<table class="table-fixed table-stripped" style="width:100%">
+    <tr>
+        <th style="width:10%"><?=t('Title')?></th>
+        <th style="width:5%"><?=t('Id')?></th>
+        <th style="width:5%"><?=t('is a child of')?></th>
+        <th style="width:5%"><?=t('Editions')?></th>
+        <th style="width:5%"><?=t('Current Edition')?></th>
+        <th style="width:9%"><?=t('Creator')?></th>
+        <th style="width:9%"><?=t('Created')?></th>
+        <th style="width:12%"><?=t('Last modifier')?></th>
+        <th style="width:9%"><?=t('Modified')?></th>
+    </tr>
+    <?php foreach ($wikipages as $wikipage): ?>
+    <tr>
+        <td>
+        <?php if (!$not_editable): ?>
+            <?=$this->url->link(t($wikipage['title']), 'WikiController', 'detail', array('plugin' => 'wiki', 'project_id' => $project['id'], 'wiki_id' => $wikipage['id']))?>
+            <?=$this->modal->confirm('trash-o', t(''), 'WikiController', 'confirm', array('plugin' => 'wiki', 'project_id' => $project['id'], 'wiki_id' => $wikipage['id']))?>
+        <?php else: ?>
+            <?=$this->url->link(t($wikipage['title']), 'WikiController', 'detail_readonly', array('plugin' => 'wiki', 'token' => $project['token'], 'wiki_id' => $wikipage['id']))?>
+        <?php endif ?>
+        </td>
+        <td>
+            <?=$wikipage['id']?>
+        </td>
+        <td>
+            <?php if (!$not_editable): ?>
+            <?=$this->url->link($wikipage['parent_id'], 'WikiController', 'detail', array('plugin' => 'wiki', 'project_id' => $project['id'], 'wiki_id' => $wikipage['parent_id']))?>
+            <?php else: ?>
+                <?=$this->url->link($wikipage['parent_id'], 'WikiController', 'detail_readonly', array('plugin' => 'wiki', 'token' => $project['token'], 'wiki_id' => $wikipage['parent_id']))?>
+            <?php endif ?>
+        </td>
+        <td><?=$wikipage['editions']?></td>
+        <td><?=$wikipage['current_edition']?></td>
+        <td><?=$this->text->e($wikipage['creator_name'] ?: $wikipage['creator_username'])?></td>
+        <td><?=$this->dt->date($wikipage['date_creation'])?></td>
+        <td><?=$this->text->e($wikipage['modifier_name'] ?: $wikipage['modifier_username'])?></td>
+        <td><?=$this->dt->date($wikipage['date_modification'])?></td>
+    </tr>
+    <?php endforeach?>
+</table>
 
 <?php else: ?>
     <p class="alert"><?=t('There are no Wiki pages for this project.')?></p>
 <?php endif?>
-</div>
-
-<!-- $this->asset->js('plugins/Wiki/Asset/Javascript/WikiChart.js') -->
